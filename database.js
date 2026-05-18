66e719cc73e9a55776a928e7a21e3acb78022727
FryMediaOfficial <frymediaofficial01@gmail.com>
2026-05-18 19:38:18 +0700

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const myAnimes = [
     {
         "title": "Otonari no Tenshi-sama ni Itsunomanika Dame Ningen ni Sareteita Ken",
         "type": "tv",
-        "genres": ["romance", "slice-of-life", "Comedy", "School"],
+        "genres": ["romance", "slice-of-life", "comedy", "school"],
         "img": "https://cdn.myanimelist.net/images/anime/1989/154843.jpg",
         "synopsis": "Mengisahkan Amane Fujimiya, seorang cowok yang kuper dan tidak memedulikan penampilannya, serta Mahiru Shiina, gadis paling cantik dan populer di sekolah yang dijuluki 'Malaikat'. Hubungan mereka dimulai secara tidak terduga setelah Amane meminjamkan payungnya kepada Mahiru di tengah hujan, yang kemudian berlanjut menjadi kedekatan manis di mana Mahiru mulai merawat Amane yang berantakan.",
         "seasons": [
@@ -11,19 +11,31 @@ const myAnimes = [
                 "name": "Season 1",
                 "year": "2023",
                 "episodes": [
-                    { "num": "1", "https://dlgan.halahgan.com/streaming.php?id=7f7b55&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-01-1080p.mp4": "" }, { "num": "2", "https://dlgan.halahgan.com/streaming.php?id=5091ba&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-02-1080p.mp4": "" }, { "num": "3", "https://dlgan.halahgan.com/streaming.php?id=88f4ef&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-03-1080p.mp4": "" },
-                    { "num": "4", "https://dlgan.halahgan.com/streaming.php?id=89620f&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-04-1080p.mp4": "" }, { "num": "5", "https://dlgan.halahgan.com/streaming.php?id=e52c24&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-05-1080p.mp4": "" }, { "num": "6", "https://dlgan.halahgan.com/streaming.php?id=a026ec&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-06-1080p.mp4": "" },
-                    { "num": "7", "https://dlgan.halahgan.com/streaming.php?id=6b15b6&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-07-1080p.mp4": "" }, { "num": "8", "https://dlgan.halahgan.com/streaming.php?id=e57af7&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-08-1080p.mp4": "" }, { "num": "9", "https://dlgan.halahgan.com/streaming.php?id=6bce1b&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-09-1080p.mp4": "" },
-                    { "num": "10", "https://dlgan.halahgan.com/streaming.php?id=0ec375&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-10-1080p.mp4": "" }, { "num": "11", "https://dlgan.halahgan.com/streaming.php?id=bc1215&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-11-1080p.mp4": "" }, { "num": "12", "https://dlgan.halahgan.com/streaming.php?id=3b51de&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-12-1080p.mp4": "" }
+                    { "num": "1", "url": "https://dlgan.halahgan.com/streaming.php?id=7f7b55&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-01-1080p.mp4" },
+                    { "num": "2", "url": "https://dlgan.halahgan.com/streaming.php?id=5091ba&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-02-1080p.mp4" },
+                    { "num": "3", "url": "https://dlgan.halahgan.com/streaming.php?id=88f4ef&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-03-1080p.mp4" },
+                    { "num": "4", "url": "https://dlgan.halahgan.com/streaming.php?id=89620f&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-04-1080p.mp4" },
+                    { "num": "5", "url": "https://dlgan.halahgan.com/streaming.php?id=e52c24&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-05-1080p.mp4" },
+                    { "num": "6", "url": "https://dlgan.halahgan.com/streaming.php?id=a026ec&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-06-1080p.mp4" },
+                    { "num": "7", "url": "https://dlgan.halahgan.com/streaming.php?id=6b15b6&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-07-1080p.mp4" },
+                    { "num": "8", "url": "https://dlgan.halahgan.com/streaming.php?id=e57af7&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-08-1080p.mp4" },
+                    { "num": "9", "url": "https://dlgan.halahgan.com/streaming.php?id=6bce1b&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-09-1080p.mp4" },
+                    { "num": "10", "url": "https://dlgan.halahgan.com/streaming.php?id=0ec375&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-10-1080p.mp4" },
+                    { "num": "11", "url": "https://dlgan.halahgan.com/streaming.php?id=bc1215&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-11-1080p.mp4" },
+                    { "num": "12", "url": "https://dlgan.halahgan.com/streaming.php?id=3b51de&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-12-1080p.mp4" }
                 ]
             },
             {
                 "name": "Season 2",
                 "year": "2026",
                 "episodes": [
-                    { "num": "1", "https://filedon.co/embed/lJ0gf9HtM1": "" }, { "num": "2", "https://filedon.co/embed/j8K3s2Q5F6": "" }, { "num": "3", "https://filedon.co/embed/3O4pDFVNRo": "" },
-                    { "num": "4", "https://filedon.co/embed/9vGEYSvF5J": "" }, { "num": "5", "https://filedon.co/embed/PjVVqgCdDL": "" }, { "num": "6", "https://filedon.co/embed/9vCmNWVObP": "" },
-                    { "num": "7", "https://filedon.co/embed/vO6FXZ4DTg": "" }
+                    { "num": "1", "url": "https://filedon.co/embed/lJ0gf9HtM1" },
+                    { "num": "2", "url": "https://filedon.co/embed/j8K3s2Q5F6" },
+                    { "num": "3", "url": "https://filedon.co/embed/3O4pDFVNRo" },
+                    { "num": "4", "url": "https://filedon.co/embed/9vGEYSvF5J" },
+                    { "num": "5", "url": "https://filedon.co/embed/PjVVqgCdDL" },
+                    { "num": "6", "url": "https://filedon.co/embed/9vCmNWVObP" },
+                    { "num": "7", "url": "https://filedon.co/embed/vO6FXZ4DTg" }
                 ]
             }
         ]
@@ -31,7 +43,7 @@ const myAnimes = [
     {
         "title": "Spy X Family",
         "type": "tv",
-        "genres": ["comedy", "action", "Spy", "Slice of Life"],
+        "genres": ["comedy", "action", "spy", "slice-of-life"],
         "img": "https://cdn.myanimelist.net/images/anime/1441/122795.jpg",
         "synopsis": "Demi menjalankan misi perdamaian dunia, seorang mata-mata terhebat dengan sandi 'Twilight' harus berpura-pura membangun sebuah keluarga. Tanpa disadari, istri yang dia nikahi adalah seorang pembunuh bayaran profesional (Yor) dan anak yang dia adopsi adalah seorang eksperimen yang bisa membaca pikiran (Anya). Mereka menyembunyikan identitas asli masing-masing demi menjaga keutuhan keluarga palsu ini.",
         "seasons": [
