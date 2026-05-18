9349eb4661ef9572949d44ad820f717cf0d8dd3e
FryMediaOfficial <frymediaofficial01@gmail.com>
2026-05-18 18:47:44 +0700

    Update database.js

--- database.js
+++ database.js
@@ -5,13 +5,25 @@ const myAnimes = [
         "type": "tv",
         "genres": ["romance", "slice-of-life"],
         "img": "https://cdn.myanimelist.net/images/anime/1989/154843.jpg",
+        "synopsis": "Mengisahkan Amane Fujimiya, seorang cowok yang kuper dan tidak memedulikan penampilannya, serta Mahiru Shiina, gadis paling cantik dan populer di sekolah yang dijuluki 'Malaikat'. Hubungan mereka dimulai secara tidak terduga setelah Amane meminjamkan payungnya kepada Mahiru di tengah hujan, yang kemudian berlanjut menjadi kedekatan manis di mana Mahiru mulai merawat Amane yang berantakan.",
         "seasons": [
             {
                 "name": "Season 1",
+                "year": "2023",
                 "episodes": [
-                    { "num": "1", "url": "https://www.youtube.com/embed/dQw4w9WgXcQ" },
-                    { "num": "2", "url": "" },
-                    { "num": "3", "url": "" }
+                    { "num": "1", "url": "" }, { "num": "2", "url": "" }, { "num": "3", "url": "" },
+                    { "num": "4", "url": "" }, { "num": "5", "url": "" }, { "num": "6", "url": "" },
+                    { "num": "7", "url": "" }, { "num": "8", "url": "" }, { "num": "9", "url": "" },
+                    { "num": "10", "url": "" }, { "num": "11", "url": "" }, { "num": "12", "url": "" }
+                ]
+            },
+            {
+                "name": "Season 2",
+                "year": "2026",
+                "episodes": [
+                    { "num": "1", "url": "" }, { "num": "2", "url": "" }, { "num": "3", "url": "" },
+                    { "num": "4", "url": "" }, { "num": "5", "url": "" }, { "num": "6", "url": "" },
+                    { "num": "7", "url": "" }
                 ]
             }
         ]
@@ -21,11 +33,36 @@ const myAnimes = [
         "type": "tv",
         "genres": ["comedy", "action"],
         "img": "https://cdn.myanimelist.net/images/anime/1441/122795.jpg",
+        "synopsis": "Demi menjalankan misi perdamaian dunia, seorang mata-mata terhebat dengan sandi 'Twilight' harus berpura-pura membangun sebuah keluarga. Tanpa disadari, istri yang dia nikahi adalah seorang pembunuh bayaran profesional (Yor) dan anak yang dia adopsi adalah seorang eksperimen yang bisa membaca pikiran (Anya). Mereka menyembunyikan identitas asli masing-masing demi menjaga keutuhan keluarga palsu ini.",
         "seasons": [
             {
-                "name": "Season 1",
+                "name": "Season 1 (Cour 1 & 2)",
+                "year": "2022",
                 "episodes": [
-                    { "num": "1", "url": "" }
+                    { "num": "1", "url": "" }, { "num": "2", "url": "" }, { "num": "3", "url": "" }, { "num": "4", "url": "" }, { "num": "5", "url": "" },
+                    { "num": "6", "url": "" }, { "num": "7", "url": "" }, { "num": "8", "url": "" }, { "num": "9", "url": "" }, { "num": "10", "url": "" },
+                    { "num": "11", "url": "" }, { "num": "12", "url": "" }, { "num": "13", "url": "" }, { "num": "14", "url": "" }, { "num": "15", "url": "" },
+                    { "num": "16", "url": "" }, { "num": "17", "url": "" }, { "num": "18", "url": "" }, { "num": "19", "url": "" }, { "num": "20", "url": "" },
+                    { "num": "21", "url": "" }, { "num": "22", "url": "" }, { "num": "23", "url": "" }, { "num": "24", "url": "" }, { "num": "25", "url": "" }
+                ]
+            },
+            {
+                "name": "Season 2",
+                "year": "2023",
+                "episodes": [
+                    { "num": "1", "url": "" }, { "num": "2", "url": "" }, { "num": "3", "url": "" }, { "num": "4", "url": "" },
+                    { "num": "5", "url": "" }, { "num": "6", "url": "" }, { "num": "7", "url": "" }, { "num": "8", "url": "" },
+                    { "num": "9", "url": "" }, { "num": "10", "url": "" }, { "num": "11", "url": "" }, { "num": "12", "url": "" }
+                ]
+            },
+            {
+                "name": "Season 3",
+                "year": "2025",
+                "episodes": [
+                    { "num": "1", "url": "" }, { "num": "2", "url": "" }, { "num": "3", "url": "" }, { "num": "4", "url": "" },
+                    { "num": "5", "url": "" }, { "num": "6", "url": "" }, { "num": "7", "url": "" }, { "num": "8", "url": "" },
+                    { "num": "9", "url": "" }, { "num": "10", "url": "" }, { "num": "11", "url": "" }, { "num": "12", "url": "" },
+                    { "num": "13", "url": "" }
                 ]
             }
         ]
